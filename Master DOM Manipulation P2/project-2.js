77a59eceef02032c8e23d4cc575244979d375dfe
tmill007 <tonymiller007@yahoo.com>
2023-03-08 10:50:46 -0600

    UPDATED JS CODE FOR PROJECT 2

--- Master DOM Manipulation P2/project-2.js
+++ Master DOM Manipulation P2/project-2.js
@@ -1,24 +1,20 @@
 // Variables
-
-let openBtn = document.getElementById('open-btn');
-let modalContainer = document.getElementById('modal-container');
-let closeBtn = document.getElementById('close-btn');
+const openBtn = document.getElementById('open-btn');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close-btn');
+const modalContainer = document.getElementById('modal-container')
 
 // Event Listeners 
-
-openBtn.addEventListener('click', function(){
-
+openBtn.addEventListener('click', function() {
     modalContainer.style.display = 'block';
-});
+})
 
 closeBtn.addEventListener('click', function() {
-
     modalContainer.style.display = 'none';
-});
+})
 
-window.addEventListener('click', function(e) {
-
+window.addEventListener('click', function(e){
     if (e.target === modalContainer) {
         modalContainer.style.display = 'none';
     }
-});
+})
